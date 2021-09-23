a1c1529d79bf3356bb98cfdcf88016b6906db30d
Will Bonnell <wbonnell123@gmail.com>
2021-09-22 23:28:35 -0500

    adding hero pattern background

--- App.svelte
+++ App.svelte
@@ -39,7 +39,8 @@
   }
 
   :global(body) {
-    background-color: #f4f5f2;
+    background-color: #dadfd1;
+    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M15 0C6.716 0 0 6.716 0 15c8.284 0 15-6.716 15-15zM0 15c0 8.284 6.716 15 15 15 0-8.284-6.716-15-15-15zm30 0c0-8.284-6.716-15-15-15 0 8.284 6.716 15 15 15zm0 0c0 8.284-6.716 15-15 15 0-8.284 6.716-15 15-15z' fill='%23f4f5f2' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
   }
 
   h1,
@@ -58,12 +59,13 @@
   .flexBox {
     display: flex;
     gap: 32px 24px;
-    margin: 64px;
+    margin: 0 64px;
+    padding-top: 64px;
     flex-wrap: wrap;
     align-items: center;
     justify-content: space-around;
     align-content: center;
-    background-color: #f4f5f2;
+    /*background-color: #f4f5f2;*/
   }
 
   :global([bgstyle="default"]) {
